b8327d05e9f8d7c4a81f7afb118e0e9c21b770b2
atotared <atota@red.software.systems>
2021-04-27 09:27:46 +0200

    Add dummy useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PostList from "./PostList";
 
 const App = () => {
+  useEffect(() => {
+    //dispatch
+    console.log("Dispatch here");
+  }, []);
+
   return (
     <div className="ui container">
       <PostList />
